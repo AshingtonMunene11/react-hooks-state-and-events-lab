e2b35c6dfbc2c7b5f1faa77dadeed4e57ea210cd
Ashington Munene <munenefrank11@gmail.com>
2025-07-17 15:54:02 +0300

    modifies components for App, item and shoppingList

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Item from "./Item";
+import React, {useState} from "react";
 
 function ShoppingList({ items }) {
+
+  const[selectedCategory, setSelectedCategory]= useState("All");
+  const itemsToDisplay = selectedCategory === "All"
+    ? items
+    : items.filter((item) => item.category === selectedCategory);
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={(e) => setSelectedCategory(e.target.value)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +20,11 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
   );
 }
-
 export default ShoppingList;
